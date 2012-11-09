3013f010d8ca0a192e1cc28d394af07f12d194e1
Julie Iskander <julie.iskander@gmail.com>
2012-11-09 12:11:54 +0200

    nmn

--- README.md
+++
@@ -1,4 +0,0 @@
-HTML-PD
-=======
-
-HTML-PD
--- js/app/Utility.js
+++
@@ -1,8 +0,0 @@
-var Utility=
-{
-    isfirefox14:function()
-    {
-        return (window.navigator.userAgent.indexOf('Firefox/14.0.1')!=-1 )
-            
-    }
-}
